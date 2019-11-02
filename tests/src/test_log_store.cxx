datatechnology/cornerstone

commit 09ae1bd1a724b1c349905162ebe0e9f473a47b1b

Random engine (#25)
* use random_device instead of seed a random engine using timestamp

* use random_device instead of seed a random engine using timestamp

include/raft_server.hxx
@@ -57,10 +57,12 @@ namespace cornerstone {
             ex_resp_handler_((rpc_handler)std::bind(&raft_server::handle_ext_resp, this, std::placeholders::_1, std::placeholders::_2)), 
             last_snapshot_(ctx->state_machine_->last_snapshot()),
             voted_servers_() {
-            uint seed = (uint)(std::chrono::system_clock::now().time_since_epoch().count() * id_);
-            std::default_random_engine engine(seed);
+            std::random_device engine;
             std::uniform_int_distribution<int32> distribution(ctx->params_->election_timeout_lower_bound_, ctx->params_->election_timeout_upper_bound_);
-            rand_timeout_ = std::bind(distribution, engine);
+            rand_timeout_ = [&distribution, &engine]() -> int32_t {
+                return distribution(engine);
+            };
+
             if (!state_) {
                 state_ = cs_new<srv_state>();
                 state_->set_term(0);

tests/src/test_buffer.cxx
@@ -39,10 +39,11 @@ void test_buffer() {
 }
 
 static void do_test(bufptr& buf) {
-    uint seed = (uint)std::chrono::system_clock::now().time_since_epoch().count();
-    std::default_random_engine engine(seed);
+    std::random_device engine;
     std::uniform_int_distribution<int32> distribution(1, 10000);
-    auto rnd = std::bind(distribution, engine);
+    auto rnd = [&distribution, &engine]() -> int32_t {
+        return distribution(engine);
+    };
 
     // store int32 values into buffer
     std::vector<int32> vals;

tests/src/test_log_store.cxx
@@ -93,10 +93,11 @@ static bool entry_equals(log_entry& entry1, log_entry& entry2) {
 }
 
 void test_log_store() {
-    uint seed = (uint)std::chrono::system_clock::now().time_since_epoch().count();
-    std::default_random_engine engine(seed);
+    std::random_device engine;
     std::uniform_int_distribution<int32> distribution(1, 10000);
-    std::function<int32()> rnd = std::bind(distribution, engine);
+    std::function<int32()> rnd = [&distribution, &engine]() -> int32_t {
+        return distribution(engine);
+    };
 
     cleanup();
     fs_log_store store(".", 100);
@@ -172,10 +173,11 @@ void test_log_store() {
 }
 
 void test_log_store_buffer() {
-    uint seed = (uint)std::chrono::system_clock::now().time_since_epoch().count();
-    std::default_random_engine engine(seed);
+    std::random_device engine;
     std::uniform_int_distribution<int32> distribution(1, 10000);
-    std::function<int32()> rnd = std::bind(distribution, engine);
+    std::function<int32()> rnd = [&distribution, &engine]() -> int32_t {
+        return distribution(engine);
+    };
 
     cleanup();
     fs_log_store store(".", 1000);
@@ -199,10 +201,11 @@ void test_log_store_buffer() {
 }
 
 void test_log_store_pack() {
-    uint seed = (uint)std::chrono::system_clock::now().time_since_epoch().count();
-    std::default_random_engine engine(seed);
+    std::random_device engine;
     std::uniform_int_distribution<int32> distribution(1, 10000);
-    std::function<int32()> rnd = std::bind(distribution, engine);
+    std::function<int32()> rnd = [&distribution, &engine]() -> int32_t {
+        return distribution(engine);
+    };
 
     cleanup();
     cleanup("tmp");
@@ -239,11 +242,12 @@ void test_log_store_pack() {
 }
 
 void test_log_store_compact_all() {
-    uint seed = (uint)std::chrono::system_clock::now().time_since_epoch().count();
-    std::default_random_engine engine(seed);
+    std::random_device engine;
     std::uniform_int_distribution<int32> distribution(1, 10000);
-    std::function<int32()> rnd = std::bind(distribution, engine);
-    
+    std::function<int32()> rnd = [&distribution, &engine]() -> int32_t {
+        return distribution(engine);
+    };
+
     cleanup();
     fs_log_store store(".", 1000);
     int cnt = rnd() % 1000 + 100;
@@ -280,10 +284,11 @@ void test_log_store_compact_all() {
 }
 
 void test_log_store_compact_random() {
-    uint seed = (uint)std::chrono::system_clock::now().time_since_epoch().count();
-    std::default_random_engine engine(seed);
+    std::random_device engine;
     std::uniform_int_distribution<int32> distribution(1, 10000);
-    std::function<int32()> rnd = std::bind(distribution, engine);
+    std::function<int32()> rnd = [&distribution, &engine]() -> int32_t {
+        return distribution(engine);
+    };
 
     cleanup();
     fs_log_store store(".", 1000);

tests/src/test_serialization.cxx
@@ -26,10 +26,11 @@ ulong long_val(int val) {
 }
 
 void test_serialization() {
-    uint seed = (uint)std::chrono::system_clock::now().time_since_epoch().count();
-    std::default_random_engine engine(seed);
+    std::random_device engine;
     std::uniform_int_distribution<int32> distribution(1, 10000);
-    auto rnd = std::bind(distribution, engine);
+    auto rnd = [&distribution, &engine]() -> int32_t {
+        return distribution(engine);
+    };
 
     ptr<srv_config> srv_conf(cs_new<srv_config>(rnd(), sstrfmt("server %d").fmt(rnd())));
     bufptr srv_conf_buf(srv_conf->serialize());

commit fb1c520243c0acfd5a16b56868da9096a5bb65b0

Fix regressions introduced by pr #25

include/raft_server.hxx
@@ -57,9 +57,10 @@ namespace cornerstone {
             ex_resp_handler_((rpc_handler)std::bind(&raft_server::handle_ext_resp, this, std::placeholders::_1, std::placeholders::_2)), 
             last_snapshot_(ctx->state_machine_->last_snapshot()),
             voted_servers_() {
-            std::random_device engine;
+            std::random_device rd;
+            std::default_random_engine engine(rd());
             std::uniform_int_distribution<int32> distribution(ctx->params_->election_timeout_lower_bound_, ctx->params_->election_timeout_upper_bound_);
-            rand_timeout_ = [&distribution, &engine]() -> int32_t {
+            rand_timeout_ = [distribution, engine]() mutable -> int32_t {
                 return distribution(engine);
             };
 

tests/Makefile.lx
@@ -1,5 +1,5 @@
 CC=g++
-CFLAGS+=-Wall -O0 -std=c++14 -g -D_GLIBCXX_USE_NANOSLEEP -I../asio/asio/include
+CFLAGS+=-Wall -O0 -std=c++17 -g -D_GLIBCXX_USE_NANOSLEEP -I../asio/asio/include
 LFLAGS=-pthread 
 VPATH=../src/:./src/
 

tests/Makefile.mac
@@ -1,5 +1,5 @@
 CC=g++
-CFLAGS+=-Wall -O0 -std=c++0x -g -D_GLIBCXX_USE_NANOSLEEP -I../asio/asio/include -DOS_FREEBSD=1
+CFLAGS+=-Wall -O0 -std=c++17 -g -D_GLIBCXX_USE_NANOSLEEP -I../asio/asio/include -DOS_FREEBSD=1
 LFLAGS=-pthread 
 VPATH=../src/:./src/
 

tests/src/test_buffer.cxx
@@ -39,9 +39,10 @@ void test_buffer() {
 }
 
 static void do_test(bufptr& buf) {
-    std::random_device engine;
+    std::random_device rd;
+    std::default_random_engine engine(rd());
     std::uniform_int_distribution<int32> distribution(1, 10000);
-    auto rnd = [&distribution, &engine]() -> int32_t {
+    auto rnd = [distribution, engine]() mutable -> int32_t {
         return distribution(engine);
     };
 

tests/src/test_everything_together.cxx
@@ -183,7 +183,7 @@ void test_raft_server_with_asio() {
             buf->put("hello");
             buf->pos(0);
             msg->log_entries().push_back(cs_new<log_entry>(0, std::move(buf)));
-            rpc_handler handler = (rpc_handler)([&client](ptr<resp_msg>& rsp1, const ptr<rpc_exception>& err1) -> void {
+            rpc_handler handler = (rpc_handler)([client](ptr<resp_msg>& rsp1, const ptr<rpc_exception>& err1) -> void {
                 assert(rsp1->get_accepted());
                 std::this_thread::sleep_for(std::chrono::milliseconds(500));
                 stop_test_cv1.notify_all();

tests/src/test_impls.cxx
@@ -357,7 +357,7 @@ public:
         t.detach();
     }
 
-    virtual void stop() {
+    virtual void stop() override {
         stopped_ = true;
         queue_.enqueue(std::make_pair(ptr<req_msg>(), ptr<async_result<ptr<resp_msg>>>()));
         std::unique_lock<std::mutex> lock(stop_lock_);
@@ -423,7 +423,7 @@ void test_raft_server() {
             buf->put("hello");
             buf->pos(0);
             msg->log_entries().push_back(cs_new<log_entry>(0, std::move(buf)));
-            rpc_handler handler = (rpc_handler)([&client](ptr<resp_msg>& rsp1, const ptr<rpc_exception>& err1) -> void {
+            rpc_handler handler = (rpc_handler)([client](ptr<resp_msg>& rsp1, const ptr<rpc_exception>& err1) -> void {
                 assert(rsp1->get_accepted());
                 std::this_thread::sleep_for(std::chrono::milliseconds(500));
                 stop_test_cv.notify_all();

tests/src/test_log_store.cxx
@@ -93,9 +93,10 @@ static bool entry_equals(log_entry& entry1, log_entry& entry2) {
 }
 
 void test_log_store() {
-    std::random_device engine;
+    std::random_device rd;
+    std::default_random_engine engine(rd());
     std::uniform_int_distribution<int32> distribution(1, 10000);
-    std::function<int32()> rnd = [&distribution, &engine]() -> int32_t {
+    std::function<int32()> rnd = [distribution, engine]() mutable -> int32_t {
         return distribution(engine);
     };
 
@@ -173,9 +174,10 @@ void test_log_store() {
 }
 
 void test_log_store_buffer() {
-    std::random_device engine;
+    std::random_device rd;
+    std::default_random_engine engine(rd());
     std::uniform_int_distribution<int32> distribution(1, 10000);
-    std::function<int32()> rnd = [&distribution, &engine]() -> int32_t {
+    std::function<int32()> rnd = [distribution, engine]() mutable -> int32_t {
         return distribution(engine);
     };
 
@@ -201,9 +203,10 @@ void test_log_store_buffer() {
 }
 
 void test_log_store_pack() {
-    std::random_device engine;
+    std::random_device rd;
+    std::default_random_engine engine(rd());
     std::uniform_int_distribution<int32> distribution(1, 10000);
-    std::function<int32()> rnd = [&distribution, &engine]() -> int32_t {
+    std::function<int32()> rnd = [distribution, engine]() mutable -> int32_t {
         return distribution(engine);
     };
 
@@ -242,9 +245,10 @@ void test_log_store_pack() {
 }
 
 void test_log_store_compact_all() {
-    std::random_device engine;
+    std::random_device rd;
+    std::default_random_engine engine(rd());
     std::uniform_int_distribution<int32> distribution(1, 10000);
-    std::function<int32()> rnd = [&distribution, &engine]() -> int32_t {
+    std::function<int32()> rnd = [distribution, engine]() mutable -> int32_t {
         return distribution(engine);
     };
 
@@ -284,9 +288,10 @@ void test_log_store_compact_all() {
 }
 
 void test_log_store_compact_random() {
-    std::random_device engine;
+    std::random_device rd;
+    std::default_random_engine engine(rd());
     std::uniform_int_distribution<int32> distribution(1, 10000);
-    std::function<int32()> rnd = [&distribution, &engine]() -> int32_t {
+    std::function<int32()> rnd = [distribution, engine]() mutable -> int32_t {
         return distribution(engine);
     };
 

tests/src/test_serialization.cxx
@@ -26,9 +26,10 @@ ulong long_val(int val) {
 }
 
 void test_serialization() {
-    std::random_device engine;
+    std::random_device rd;
+    std::default_random_engine engine(rd());
     std::uniform_int_distribution<int32> distribution(1, 10000);
-    auto rnd = [&distribution, &engine]() -> int32_t {
+    auto rnd = [distribution, engine]() mutable -> int32_t {
         return distribution(engine);
     };
 